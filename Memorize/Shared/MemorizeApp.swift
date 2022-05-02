d282093d5f441b9e6657492bf53b6e65559fb166
Viraj Lakshitha Bandara <viraj-lakshitha@Viraj-MacBook-Air.local>
2022-05-02 09:40:23 +0530

    Initialize Swift UI project and works on basic views

---
+++ Memorize/Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Viraj Lakshitha Bandara on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
